0961e66fbed6f826806e76a8fa360540e09601b0
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2023-06-12 21:06:43 -0500

    canvas y guía inicial

--- js/main.js
+++ js/main.js
@@ -1,1 +1,35 @@
 // JavaScript principal
+
+/**
+ * @type HTMLCanvasElement
+ */
+
+const canvas = document.getElementById("canvas");
+const guide = document.getElementById("guide");
+const colorInput = document.getElementById("colorInput");
+const toggleGuide = document.getElementById("toggleGuide");
+const clearButton = document.getElementById("clearButton");
+const drawingContext = canvas.getContext("2d");
+
+const CELL_SIDE_COUNT = 5;
+const cellPixelLenght = canvas.width / CELL_SIDE_COUNT;
+const colorHistory = {};
+
+// Color predeterminado
+colorInput.value = "#009578";
+
+// Inicializar fondo canvas
+drawingContext.fillStyle = "#ffffff";
+drawingContext.fillRect(0, 0, canvas.width, canvas.height);
+
+// Configurar guía
+{
+    guide.style.width = `${canvas.width}px`;
+    guide.style.height = `${canvas.height}px`;
+    guide.style.gridTemplateColumns = `repeat(${CELL_SIDE_COUNT}, 1fr)`;
+    guide.style.gridTemplateRows = `repeat(${CELL_SIDE_COUNT}, 1fr)`;
+
+    [... Array(CELL_SIDE_COUNT ** 2)].forEach(() =>
+        guide.insertAdjacentHTML("beforeend", "<div></div>")
+    );
+}
